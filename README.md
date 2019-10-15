e7df468bb5db720e362d6a7825f2f86053fe3bd9
haizhi <haizhi@hz.local>
2019-10-15 11:27:56 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-manage
-Purchasing management system
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+/**
+ * 应用根组件
+ */
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      init
+    </div>
+  );
+}
+
+export default App;
