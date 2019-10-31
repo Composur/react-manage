c1717fe17e70eaff08e8ed36263972fe20213d15
Composur <xutong1107@gmail.com>
2019-10-31 20:28:58 +0800

    feat(商品管理):50%

---
+++ client/build/precache-manifest.1e30bb1ebe205138cc50fed678b38c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684d4389d22085abfb24962771df929f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76dd9b852012cc5e420f",
+    "url": "/static/css/2.e98b119f.chunk.css"
+  },
+  {
+    "revision": "5f315c48906e0232ddfc",
+    "url": "/static/css/main.139bec77.chunk.css"
+  },
+  {
+    "revision": "76dd9b852012cc5e420f",
+    "url": "/static/js/2.157c993b.chunk.js"
+  },
+  {
+    "revision": "5f315c48906e0232ddfc",
+    "url": "/static/js/main.d79ed7cf.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "96c8b37aad1423999607fe72d82f4a8f",
+    "url": "/static/media/bg.96c8b37a.png"
+  }
+]);
