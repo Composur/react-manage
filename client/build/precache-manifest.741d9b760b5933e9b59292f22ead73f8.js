bc08fca953219f814fbd6ac512f1365db60eff5c
Composur <xutong1107@gmail.com>
2019-11-07 21:42:23 +0800

    build

---
+++ client/build/precache-manifest.741d9b760b5933e9b59292f22ead73f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6379142fa4044e5537f6c7d43987fa55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24108a8a534e3f5d53f",
+    "url": "/static/css/2.29aa9ee3.chunk.css"
+  },
+  {
+    "revision": "e9cdb4d40ab77568830f",
+    "url": "/static/css/main.139bec77.chunk.css"
+  },
+  {
+    "revision": "d24108a8a534e3f5d53f",
+    "url": "/static/js/2.54e3dd11.chunk.js"
+  },
+  {
+    "revision": "e9cdb4d40ab77568830f",
+    "url": "/static/js/main.7ebae24f.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "96c8b37aad1423999607fe72d82f4a8f",
+    "url": "/static/media/bg.96c8b37a.png"
+  }
+]);
