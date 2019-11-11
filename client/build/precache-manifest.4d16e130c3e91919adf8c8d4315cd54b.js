783efbe03b93b64cfc150650f59621add1ac812f
Composur <xutong1107@gmail.com>
2019-11-11 23:23:56 +0800

    feat:上传上传的图片

---
+++ client/build/precache-manifest.4d16e130c3e91919adf8c8d4315cd54b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaa1c92442c4df75dd9a7522540f258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbb44aab42790273f00",
+    "url": "/static/css/2.29d910bb.chunk.css"
+  },
+  {
+    "revision": "b761a6a34d65c98d226c",
+    "url": "/static/css/main.139bec77.chunk.css"
+  },
+  {
+    "revision": "ccbb44aab42790273f00",
+    "url": "/static/js/2.9f3aa762.chunk.js"
+  },
+  {
+    "revision": "b761a6a34d65c98d226c",
+    "url": "/static/js/main.561b06fc.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "96c8b37aad1423999607fe72d82f4a8f",
+    "url": "/static/media/bg.96c8b37a.png"
+  }
+]);
