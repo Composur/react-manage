d31bfabf25dceeccb2db97fe6cfe68380271437b
Composur <xutong1107@gmail.com>
2019-11-19 21:26:22 +0800

    build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3074fd9f82545dd01b8eb69a7feece83.js"
+  "/precache-manifest.6195280ee29e716af5d077fda8b803ec.js"
 );
 
 self.addEventListener('message', (event) => {
