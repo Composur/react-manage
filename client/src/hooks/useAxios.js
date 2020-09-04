3334dba8d43912ec87f16db94123da9a68f0edb0
composure <xutong1107@gmail.com>
2020-09-04 22:50:04 +0800

    feat: useAxios

---
+++ client/src/hooks/useAxios.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Object} config 请求配置
+ * @param {dependencies} dependencies 
+ * @returns {Array} 
+ */
+export default (config, dependencies) => {
+	const [data, setData] = useState()
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState()
+
+	useEffect(() => {
+		setLoading(true);
+		Axios.request(config)
+			.then(setData)
+			.catch(setError)
+			.finally(() => setLoading(false))
+	}, [dependencies])
+
+	return [{ data, loading, error }]
+}
